70a05c90bb250197d60673daaa3973557af2173d
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-05-08 21:38:58 +0200

    added all app components

---
+++ src/app/components/app/AppSearch.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { headingLarge } from "@/app/styles/Mixins";
+import { styled } from "styled-components";
+
+const SearchIcon = () => {
+  return (
+    <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M27.613 25.72 23.08 21.2a10.56 10.56 0 0 0 2.253-6.533C25.333 8.776 20.558 4 14.667 4S4 8.776 4 14.667c0 5.89 4.776 10.666 10.667 10.666A10.56 10.56 0 0 0 21.2 23.08l4.52 4.533a1.333 1.333 0 0 0 1.893 0 1.333 1.333 0 0 0 0-1.893ZM6.667 14.667a8 8 0 1 1 16 0 8 8 0 0 1-16 0Z"
+        fill="#FFF"
+      />
+    </svg>
+  );
+};
+
+const SearchField = styled.input`
+  padding-bottom: 1rem;
+  background-color: transparent;
+  color: ${(props) => props.theme.white};
+  caret-color: ${(props) => props.theme.red};
+  width: 484px;
+  ${headingLarge}
+
+  &:active {
+    border-bottom: 1px solid ${(props) => props.theme.greyishBlue};
+  }
+`;
+
+const AppSearchContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+
+const AppSearch = () => {
+  return (
+    <AppSearchContainer>
+      <SearchIcon />
+      <SearchField placeholder="Search for movies or TV series" />
+    </AppSearchContainer>
+  );
+};
+
+export default AppSearch;
---
+++ src/app/styles/Theme.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  mobile: "480px",
+  desktop: "1440px",
+
+  red: "hsl(0, 97%, 63%)",
+  darkBlue: "hsl(223, 30%, 9%)",
+  greyishBlue: "hsl(223, 23%, 46%)",
+  semiDarkBlue: "hsl(223, 36%, 14%)",
+  white: "hsl(0, 0%, 100%)",
+};
